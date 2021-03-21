e4976001b44500080ae27e7464862c27478b2609
Waranthorn Chansawang <waranthorn_c@outlook.com>
2021-03-22 01:07:40 +0700

    Update bow.py

--- SENIOR-HOMEWORK/homework-PGam/bow/bow.py
+++ SENIOR-HOMEWORK/homework-PGam/bow/bow.py
@@ -10,16 +10,32 @@ def fhash(word, M, G=37):
         result = sum_ord_num%M
     return result
 
+def duplicates(numbers_list):
+    store=[]
+    checked=[]
+    for i in range(len(numbers_list)):
+        counter = 1 
+        for j in range(i+1,len(numbers_list)):
+            if numbers_list[i] not in checked and numbers_list[j]==numbers_list[i] :
+                counter +=1 
+        if counter > 1 :
+            store.append(numbers_list[i])
+            checked.append(numbers_list[i])
+    return store
+
 word_list = []
 for word in str3.split():
     # print(word)
     word_list.append(word)
 
+fh_word_list = []
 for word in word_list:
-    f_word = fhash(word, 4)
-    # print(f_word)
-    print(word, f_word)
-    
-#     for letter in word:
-#         print(letter)
+    fh_word = fhash(word, 4)
+    fh_word_list.append(fh_word)
+    # print(word, fh_word)
 
+print(fh_word_list.count(fh_word))
+
+# print(duplicates(fh_word_list))
+
+# BoW = []
